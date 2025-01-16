6f25560ae2dfa033ea6fb6d7aed32950301716fb
Tom Schrogl <tom-schrogl@web.de>
2025-01-16 10:43:21 +0100

    add several enpoints to get schueler endpoint: /schueler with 4 parameters returns a certain schueler endpoint: /schuelerbyid with id parameter endpoint: /all returns all entries of schueler table
    "Something fixed" -https://whatthecommit.com/b384df81d6351ea010f4335667ce7757

--- spring-boot/src/main/java/com/spring_boot/StudentFormController.java
+++ spring-boot/src/main/java/com/spring_boot/StudentFormController.java
@@ -1,22 +1,48 @@
 package com.spring_boot;
 
+import com.spring_boot.schueler.Schueler;
+import com.spring_boot.schueler.SchuelerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student-form")
 public class StudentFormController {
 
     private final StudentFormService studentFormService;
+    private final SchuelerService schuelerService;
 
     @Autowired
     public StudentFormController(StudentFormService studentFormService) {
         this.studentFormService = studentFormService;
+        schuelerService = studentFormService.getSchuelerService();
     }
 
     @GetMapping("/")
     public String getRootResponse() {
         return "";
+    }
+
+    @GetMapping("/schueler")
+    public Schueler getSchueler(String vorname, String familienname, String geburtsdatum, String email) {
+        Schueler schueler = new Schueler();
+        schueler.setVorname(vorname);
+        schueler.setFamilienname(familienname);
+        schueler.setGeburtsdatum(geburtsdatum);
+        schueler.setEmail(email);
+        return schuelerService.getOrCreateSchueler(schueler);
+    }
+
+    @GetMapping("/schuelerbyid")
+    public Schueler getSchuelerByID(String id) {
+        return schuelerService.getSchuelerByID(id);
+    }
+
+    @GetMapping("/all")
+    public List<Schueler> getAllSchueler() {
+        return schuelerService.getAllSchueler();
     }
 
     @PostMapping("/submit")
